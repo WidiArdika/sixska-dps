6aa0b9c444b3553190962a52f915583f70e6e53f
WidiArdika <dewidiardika@gmail.com>
2025-07-15 22:31:07 +0800

    vite di blade

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SMK Negeri 6 Denpasar</title>
     <link rel="icon" href="{{ asset('images/SMKN6.svg') }}" type="image/x-icon">
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script> --}}
 </head>
 
